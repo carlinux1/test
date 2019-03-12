cb76fbadcc585ade3baa6d607e61966c56b54de7
carloagostini <carlo.agostini@gmail.com>
2019-03-12 14:09:37 +0100

    invocation file_open function

--- src/main.c
+++ src/main.c
@@ -3,10 +3,13 @@
 #include <string.h>
 
 #include "my_include.h"
+#include "file.h"
 
 int main(int agrc,char*argv[])
 {
 
-printf("main %s\n",argv[0]);
+  printf("main %s\n",argv[0]);
+  file_open("pippo.txt");
+
 return 0;
 }
